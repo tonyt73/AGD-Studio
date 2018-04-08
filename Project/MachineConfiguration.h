840c76fc4d8b3c6bbfd7e0be2092a624df55c822
Tony Thompson <tonyt73@gmail.com>
2018-04-08 17:27:34 +1000

    Initial machine config class

---
+++ Project/MachineConfiguration.h
@@ -0,0 +1,17 @@
+//---------------------------------------------------------------------------
+#ifndef MachineConfigurationH
+#define MachineConfigurationH
+//---------------------------------------------------------------------------
+#include "System/JsonFile.h"
+//---------------------------------------------------------------------------
+class MachineConfiguration : public JsonFile
+{
+private:
+
+
+public:
+            __fastcall  MachineConfiguration();
+
+};
+//---------------------------------------------------------------------------
+#endif
