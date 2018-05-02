c7287a7608dc57034c428588be7f95c9fb0d7f42
Tony Thompson <tonyt73@gmail.com>
2018-05-02 20:08:52 +1000

    Moved sounds fx file to game folder

--- Project/TextDocuments.cpp
+++ Project/TextDocuments.cpp
@@ -12,7 +12,7 @@ __fastcall TextDocument::TextDocument(const String& name)
     m_Type = "Text";
     m_SubType = "Plain";
     m_Extension = "txt";
-    m_Folder = "Assets\\Files";
+    m_Folder = "Game\\Files";
     RegisterProperty("Name", "Details", "The name of the document");
     RegisterProperty("Filename", "File", "The name and path of the file");
     m_File = GetFile();
@@ -78,9 +78,17 @@ __fastcall SfxDocument::SfxDocument(const String& name)
 : TextDocument(name)
 {
     m_SubType = "SoundFx";
-    m_Folder = "Assets\\Sounds";
+    m_Folder = "Game\\Sounds";
     m_Extension = "sfx";
     RegisterProperty("Name", "Details", "The name of the SoundFx definitions file");
+    auto file = GetFile();
+    if (m_Name != "unnamed" && !System::File::Exists(file))
+    {
+        // create the file and add an AGD header
+        auto date = DateTimeToStr(Now());
+        auto header = ";\r\n; " + System::Path::ProjectName + "\r\n; An AGDX game\r\n; Created: " + date + "\r\n; Simply place a Sound Fx file name per line. Comments ';' are stripped before compilation\r\n;";
+        System::File::WriteText(file, header);
+    }
     m_File = GetFile();
 }
 //---------------------------------------------------------------------------
