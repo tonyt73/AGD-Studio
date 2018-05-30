a1fa45ea20d7b3fe1cc7154b80235c03342313a6
Tony Thompson <tonyt73@gmail.com>
2018-05-30 18:54:52 +1000

    Moved frames into specific folders

--- AGDX Studio.cpp
+++ AGDX Studio.cpp
@@ -6,24 +6,24 @@
 #include <Vcl.Themes.hpp>
 //---------------------------------------------------------------------------
 //USEFORM("Forms\fNewImage.cpp", frmNewImage);
-USEFORM("Frames\fEditorImage.cpp", frmEditorImage); /* TFrame: File Type */
-USEFORM("Frames\fEditorCode.cpp", frmEditorCode); /* TFrame: File Type */
+USEFORM("Frames\MapEditor\fEditorMap.cpp", frmEditorMap); /* TFrame: File Type */
+USEFORM("Frames\ImageEditor\fEditorImage.cpp", frmEditorImage); /* TFrame: File Type */
+USEFORM("Frames\CodeEditor\fEditorCode.cpp", frmEditorCode); /* TFrame: File Type */
+USEFORM("Frames\ImportDialog\fImportDialog.cpp", frmImportDialog); /* TFrame: File Type */
+USEFORM("Frames\IDE\fIDE.cpp", frmIDE); /* TFrame: File Type */
+USEFORM("Frames\MusicEditor\fEditorMusic.cpp", frmEditorMusic); /* TFrame: File Type */
 USEFORM("Forms\fSettings.cpp", frmSettings);
-USEFORM("Frames\fIDE.cpp", frmIDE); /* TFrame: File Type */
-USEFORM("Frames\fEditorMusic.cpp", frmEditorMusic); /* TFrame: File Type */
-USEFORM("Frames\fEditorMap.cpp", frmEditorMap); /* TFrame: File Type */
 USEFORM("Forms\fNewImage.cpp", frmNewImage);
 USEFORM("Forms\fMain.cpp", frmMain);
 USEFORM("Forms\fAbout.cpp", frmAbout);
+USEFORM("Frames\ImageEditor\fPaletteBitmap.cpp", frmPaletteBitmap); /* TFrame: File Type */
 USEFORM("Frames\ImageEditor\fPaletteAttribute.cpp", frmPaletteAttribute); /* TFrame: File Type */
 USEFORM("Frames\ImageEditor\fToolbarShape.cpp", frmToolbarShape); /* TFrame: File Type */
 USEFORM("Frames\ImageEditor\fToolbar.cpp", frmToolbar); /* TFrame: File Type */
-USEFORM("Frames\fSelectionImage.cpp", SelectionImageFrame); /* TFrame: File Type */
-USEFORM("Frames\fMultiImageView.cpp", MultiImageViewFrame); /* TFrame: File Type */
-USEFORM("Frames\fImportDialog.cpp", frmImportDialog); /* TFrame: File Type */
-USEFORM("Frames\fWelcomeDialog.cpp", frmWelcomeDialog); /* TFrame: File Type */
-USEFORM("Frames\fSelectionPanel.cpp", SelectionPanelFrame); /* TFrame: File Type */
-USEFORM("Frames\ImageEditor\fPaletteBitmap.cpp", frmPaletteBitmap); /* TFrame: File Type */
+USEFORM("Frames\WelcomeDialog\fSelectionPanel.cpp", SelectionPanelFrame); /* TFrame: File Type */
+USEFORM("Frames\ImageEditor\fSelectionImage.cpp", SelectionImageFrame); /* TFrame: File Type */
+USEFORM("Frames\ImageEditor\fMultiImageView.cpp", MultiImageViewFrame); /* TFrame: File Type */
+USEFORM("Frames\WelcomeDialog\fWelcomeDialog.cpp", frmWelcomeDialog); /* TFrame: File Type */
 //---------------------------------------------------------------------------
 int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
