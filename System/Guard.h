e4591fe39138fc9d20e91d193fd419e51c0182f6
Tony Thompson <tonyt73@gmail.com>
2019-01-02 18:15:19 +1000

    Update Guard.h

--- System/Guard.h
+++ System/Guard.h
@@ -2,6 +2,12 @@
 #ifndef GuardH
 #define GuardH
 //---------------------------------------------------------------------------
+// A guard to use in a single function
+// Sets the supplied guard flag to true while the function is running
+// Check the flag in other functions to stop processing.
+// Generally used when the UI is updating and you don't want to trigger
+// and more control updates
+//---------------------------------------------------------------------------
 class Guard
 {
 private:
@@ -10,5 +16,6 @@ public:
         __fastcall  Guard(bool& flag);
         __fastcall ~Guard();
 };
+#define GUARD(x) auto g = Guard(x)
 //---------------------------------------------------------------------------
 #endif
