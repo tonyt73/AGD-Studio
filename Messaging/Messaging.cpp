6556cb6c14d1216f7bba94354c6257c4423e3991
Tony Thompson <tonyt73@gmail.com>
2019-01-03 19:51:35 +1000

    Rad Studio 10.3 project file
    Compiler is not playing well with the code

--- Messaging/Messaging.cpp
+++ Messaging/Messaging.cpp
@@ -5,6 +5,53 @@
 #pragma package(smart_init)
 //---------------------------------------------------------------------------
 #if defined(_WIN32) || defined(__APPLE__) || defined(__linux__)
-auto ::Messaging::Bus::m_Handlers = new ::Messaging::Bus::SubscriptionsMap();
+unsigned int ::Messaging::Bus::s_NextId = 0;
+::Messaging::Bus::SubscriptionsMap* ::Messaging::Bus::s_Handlers = nullptr;
+//auto ::Messaging::Bus::m_Handlers = new ::Messaging::Bus::SubscriptionsMap();
+//---------------------------------------------------------------------------
+__fastcall Messaging::Registrar::Registrar()
+{
+}
+//---------------------------------------------------------------------------
+__fastcall Messaging::Registrar::~Registrar()
+{
+    Unsubscribe();
+}
+//---------------------------------------------------------------------------
+// unsubscribe a handler from a message type
+void Messaging::Bus::Unsubscribe(unsigned int subscriptionId)
+{
+    if (s_Handlers == nullptr)
+        return;
+
+    for (auto& subscriptions : *s_Handlers)
+    {
+        if (subscriptions.second != nullptr)
+        {
+            subscriptions.second->erase(std::remove_if(subscriptions.second->begin(), subscriptions.second->end(),
+                [&](const std::unique_ptr<Subscription_>& sub) { return (subscriptionId == sub->SubscriptionId); }), subscriptions.second->end());
+            if (subscriptions.second->size() == 0)
+            {
+                // remove the subscriptions from the handlers list
+                s_Handlers->erase(subscriptions.first);
+            }
+        }
+        else
+        {
+            s_Handlers->erase(subscriptions.first);
+        }
+    }
+}
+//---------------------------------------------------------------------------
+void __fastcall Messaging::Registrar::Unsubscribe()
+{
+    for (auto id : m_SubscriptionIds)
+    {
+        ::Messaging::Bus::Unsubscribe(id);
+    }
+    m_SubscriptionIds.clear();
+}
+//---------------------------------------------------------------------------
 #endif // defined(_WIN32) || defined(__APPLE__) || defined(__linux__)
 //---------------------------------------------------------------------------
+
--- Messaging/Messaging.h
+++ Messaging/Messaging.h
@@ -3,6 +3,7 @@
 #define MessagingH
 //---------------------------------------------------------------------------
 #if defined(_WIN32) || defined(__APPLE__) || defined(__linux__)
+#include <assert.h>
 #include <list>
 #include <memory>
 #include <unordered_map>
@@ -17,85 +18,113 @@ namespace Messaging
 {
 class Bus
 {
+friend class Registrar;
 private:
+    static unsigned int s_NextId;
     // Non-templated base class
     // Makes storing the templated objects easier
     class Subscription_
     {
+    private:
+        unsigned int    m_SubscriptionId;
+
     public:
-        virtual ~Subscription_() {};
+                        Subscription_(unsigned int id) : m_SubscriptionId(id) {};
+        virtual        ~Subscription_() {};
+
+        __property unsigned int SubscriptionId = { read = m_SubscriptionId };
     };
+
     // Template specialization of a message subscription
     template <class T>
     class Subscription : public Subscription_
     {
     private:
-        std::function<void (const T&)> handler;
+        std::function<void (const T&)>  m_Handler;
     public:
-        __fastcall Subscription(std::function<void (const T&)> handler)
-        : handler(handler)
+        Subscription(std::function<void (const T&)> handler, unsigned int subscriptionId)
+        : Subscription_(subscriptionId)
+        , m_Handler(handler)
         {
         }
-        __property std::function<void (const T&)> Handler = { read = handler };
+
+        void Execute(const T& message)
+        {
+            if (m_Handler)
+            {
+                m_Handler(message);
+            }
+            else
+            {
+                assert("Handler is invalid");
+            }
+        }
     };
 
     // the map of message subscription handlers
     typedef std::list<std::unique_ptr<Subscription_>> Subscriptions;
     typedef std::unordered_map<std::type_index, std::unique_ptr<Subscriptions>> SubscriptionsMap;
-    static SubscriptionsMap* m_Handlers;
+    static SubscriptionsMap* s_Handlers;
 
-public:
+protected:
     // subscribe a handler to a templated message type
     template <class T>
-    static void const Subscribe(std::function<void (const T&)> handler)
+    static unsigned int Subscribe(std::function<void (const T&)> handler)
     {
-        auto& subscriptions = (*m_Handlers)[typeid(T)];
+        if (s_Handlers == nullptr)
+        {
+            s_Handlers = new SubscriptionsMap();
+        }
+
+        auto& subscriptions = (*s_Handlers)[typeid(T)];
         if (subscriptions == nullptr)
         {
             // add a new subscriptions list to the type handlers list
             subscriptions = make_unique<Subscriptions>();
-            (*m_Handlers)[typeid(T)] = std::move(subscriptions);
+            (*s_Handlers)[typeid(T)] = std::move(subscriptions);
         }
 
-        // add the handler to the subscriptions list for the type
-        auto subscription = make_unique<Subscription<T>>(handler);
+        // add the handler to the subscriptions list for the type and assign a new id
+        auto subscription = make_unique<Subscription<T>>(handler, ++s_NextId);
         subscriptions->push_back(std::move(subscription));
+        return s_NextId;
     }
 
-    // unsubscribe a handler from a templated message type
-    template <class T>
-    static void Unsubscribe(std::function<void (const T&)> handler)
-    {
-        std::unique_ptr<Subscriptions>& subscriptions = (*m_Handlers)[typeid(T)];
-        if (subscriptions != nullptr)
-        {
-            const auto& subscription = find(subscriptions->begin(), subscriptions->end(), handler);
-            if (subscription != subscriptions->end())
-            {
-                // remove the subscription of the function handler from the subscriptions list
-                subscriptions->erase(subscription);
-            }
-            if (subscriptions->size() == 0)
-            {
-                // remove the subscriptions from the handlers list
-                m_Handlers->erase(typeid(T));
-            }
-        }
-    }
+    static void Unsubscribe(unsigned int subscriptionId);
 
+public:
     // publish a message to all subscribers of the type
     template <class T>
     static void Publish(const T& message)
     {
-        auto& subscriptions = (*m_Handlers)[typeid(T)];
+        if (s_Handlers == nullptr)
+            return;
+        auto& subscriptions = (*s_Handlers)[typeid(T)];
         if (subscriptions != nullptr)
         {
             for (const auto& subscription : *subscriptions)
             {
-                ((Subscription<T>*)(subscription.get()))->Handler(message);
+                ((Subscription<T>*)(subscription.get()))->Execute(message);
             }
         }
     }
+};
+//---------------------------------------------------------------------------
+class Registrar
+{
+private:
+    std::list<unsigned int> m_SubscriptionIds;
+
+public:
+            __fastcall  Registrar();
+            __fastcall ~Registrar();
+
+    template <class T>
+    void    __fastcall  Subscribe(std::function<void (const T&)> handler)
+    {
+        m_SubscriptionIds.push_back(::Messaging::Bus::Subscribe(handler));
+    }
+    void    __fastcall  Unsubscribe();
 };
 //---------------------------------------------------------------------------
 } // namespace Messaging
