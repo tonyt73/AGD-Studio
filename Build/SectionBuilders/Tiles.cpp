ca5dbd607b900201c723c1c62469bcc4d4a92ed2
Tony Thompson <tonyt73@gmail.com>
2019-01-12 21:16:25 +1000

    Added Tiles Section export

--- Build/SectionBuilders/Tiles.cpp
+++ Build/SectionBuilders/Tiles.cpp
@@ -1,8 +1,14 @@
 //---------------------------------------------------------------------------
 #include "agdx.pch.h"
-#include "Tiles.h"
+#include "Build/SectionBuilders/Tiles.h"
+#include "Project/DocumentManager.h"
+#include "Project/ImageDocuments.h"
+#include "Graphics/GraphicsMode.h"
+#include "Graphics/Image.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
+//---------------------------------------------------------------------------
+const String BlockTypes[] = { "Empty", "Platform", "Wall", "Ladder", "Fodder", "Deadly", "Custom" };
 //---------------------------------------------------------------------------
 __fastcall SectionBuilders::Tiles::Tiles()
 {
@@ -14,6 +20,34 @@ __fastcall SectionBuilders::Tiles::~Tiles()
 //---------------------------------------------------------------------------
 void __fastcall SectionBuilders::Tiles::Execute()
 {
+    const auto& dm = theDocumentManager;
+    DocumentList images;
+    dm.GetAllOfType("Image", images);
+    for (auto image : images)
+    {
+        auto tile = dynamic_cast<TileDocument*>(image);
+        if (tile != nullptr)
+        {
+            String line = "DEFINEBLOCK ";
+            line += BlockTypes[StrToInt(tile->GetLayer("blocktype"))];
+            line += " ";
+            const auto& gm = (*(theDocumentManager.ProjectConfig()->MachineConfiguration().GraphicsMode()));
+            // make an image canvas
+            auto image = std::make_unique<Agdx::Image>(tile, gm);
+            image->ChangeFrame(0);
+            auto data = image->GetExportNativeFormat();
+            // export the machine graphics data
+            for (auto byte : data)
+            {
+                line += IntToStr(byte) + " ";
+            }
+            AddLine(line);
+            LineBreak();
+        }
+    }
+
+    // no tiles is ok
+    Success();
 }
 //---------------------------------------------------------------------------
 
