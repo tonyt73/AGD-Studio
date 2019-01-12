66bc36477806a86c97c163c6394aa04d0677c0bd
Tony Thompson <tonyt73@gmail.com>
2019-01-12 17:19:59 +1000

    Implement Events AGD output

--- Build/StringBuilders/Events.cpp
+++ Build/StringBuilders/Events.cpp
@@ -1,7 +1,10 @@
 //---------------------------------------------------------------------------
 #include "agdx.pch.h"
+#include <System.StrUtils.hpp>
 #include "Build/StringBuilders/Events.h"
 #include "Project/FileDefinitions.h"
+#include "System/File.h"
+#include "System/Path.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 //---------------------------------------------------------------------------
@@ -15,12 +18,31 @@ __fastcall StringBuilders::Events::~Events()
 //---------------------------------------------------------------------------
 void __fastcall StringBuilders::Events::Execute()
 {
+    auto gamePath = System::Path::Project;
     auto definitions = std::make_unique<FileDefinitions>();
     for (const auto& definition : definitions->GetDefinitions())
     {
         if (definition.Type == "Event")
         {
-
+            auto file = System::File::Combine(gamePath, definition.Filename + ".event");
+            if (System::File::Exists(file))
+            {
+                auto lines = System::File::ReadLines(file);
+                AddLine(definition.Section);
+                for (auto line : lines)
+                {
+                    if (line.Trim().UpperCase().Pos(definition.Section) == 0)
+                    {
+                        AddLine(line);
+                    }
+                }
+                LineBreak();
+                Success();
+            }
+            else
+            {
+                Failure("Failed to find the event file: " + file);
+            }
         }
     }
 }
