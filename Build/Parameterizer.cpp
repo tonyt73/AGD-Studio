ff24a8cde9c915ce4ff570c89ef8f1b7eb7e4a71
Tony Thompson <tonyt73@gmail.com>
2019-01-23 13:13:44 +1000

    Parameterization class
    replaces %tagged% parameters with their real value

---
+++ Build/Parameterizer.cpp
@@ -0,0 +1,20 @@
+//---------------------------------------------------------------------------
+#include "agdx.pch.h"
+#include "Build/Parameterizer.h"
+#include "Project/DocumentManager.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+//---------------------------------------------------------------------------
+String Parameter::ization(const String& parameters)
+{
+    auto subbedParameters = parameters;
+    const auto& mc = theDocumentManager.ProjectConfig()->MachineConfiguration();
+    auto gameName = System::Path::ProjectName;
+    auto gamePath = System::Path::Project;
+
+    subbedParameters = StringReplace(subbedParameters, "%GamePath%", gamePath, TReplaceFlags(rfReplaceAll || rfIgnoreCase));
+    subbedParameters = StringReplace(subbedParameters, "%GameName%", gameName, TReplaceFlags(rfReplaceAll || rfIgnoreCase));
+
+    return subbedParameters;
+}
+//---------------------------------------------------------------------------
