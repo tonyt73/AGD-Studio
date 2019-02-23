0ac7e1ba5e90d885ba53ee01291242e22fb3b5b5
Tony Thompson <tonyt73@gmail.com>
2019-02-23 21:37:19 +1000

    Update Window.cpp
    Fixed AGD window code

--- Build/AgdSection/Window.cpp
+++ Build/AgdSection/Window.cpp
@@ -18,7 +18,7 @@ __fastcall SectionBuilders::Window::~Window()
 void __fastcall SectionBuilders::Window::Execute()
 {
     const auto& Window = (WindowDocument*)theDocumentManager.Get("Window", "Definition", "Window");
-    AddLine("DEFINEWINDOW " + IntToStr((int)Window->Rect.Left) + " " + IntToStr((int)Window->Rect.Right) + " " + IntToStr((int)Window->Rect.Height()) + " " + IntToStr((int)Window->Rect.Width()));
+    AddLine("DEFINEWINDOW " + IntToStr((int)Window->Rect.Top) + " " + IntToStr((int)Window->Rect.Left) + " " + IntToStr((int)Window->Rect.Height()) + " " + IntToStr((int)Window->Rect.Width()));
     LineBreak();
     Success();
 }
