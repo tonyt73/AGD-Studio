60285d353b81b18ac71d01897dcbc9c30943caee
Tony Thompson <tonyt73@gmail.com>
2019-03-17 18:13:56 +1000

    Create Build Installers.bat

---
+++ Install/Build Installers.bat
@@ -0,0 +1,7 @@
+@rem 
+@rem Build Installers script
+@rem  
+
+@rem Build Installer Packages
+"C:\Program Files (x86)\NSIS\makensis.exe" "AGDx Studio 64.nsi"
+"C:\Program Files (x86)\NSIS\makensis.exe" "AGDx Studio 32.nsi"
