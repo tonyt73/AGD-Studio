184623c03d415d2654de9cc8538e320b560aa34e
Tony Thompson <tonyt73@gmail.com>
2019-03-17 18:14:27 +1000

    Update fEditorWindow.cpp
    Added better Themeing

--- Frames/WindowEditor/fEditorWindow.cpp
+++ Frames/WindowEditor/fEditorWindow.cpp
@@ -29,7 +29,7 @@ __fastcall TfrmEditorWindow::~TfrmEditorWindow()
 //---------------------------------------------------------------------------
 void __fastcall TfrmEditorWindow::FrameResize(TObject *Sender)
 {
-    Color = ThemeManager::Background;//StyleServices()->GetStyleColor(scGenericGradientBase);
+    Color = ThemeManager::Background;
     auto s = 8;
     for (; s <= 128; s++)
     {
@@ -56,13 +56,13 @@ void __fastcall TfrmEditorWindow::DrawView()
     // draw the window area
     if (m_View != nullptr)
     {
-        m_View->Canvas->Brush->Color = clGray;
+        m_View->Canvas->Brush->Color = ThemeManager::Background;
         m_View->Canvas->FillRect(TRect(0, 0, m_View->Width, m_View->Height));
         for (auto y = m_Document->Top; y <= m_Document->Bottom; y++)
         {
             for (auto x = m_Document->Left; x <= m_Document->Right; x++)
             {
-                m_View->Canvas->Pixels[x][y] = (y + x) % 2 ? clRed : clWhite;
+                m_View->Canvas->Pixels[x][y] = (y + x) % 2 ? ThemeManager::Highlight : ThemeManager::Foreground;
             }
         }
 
@@ -71,17 +71,21 @@ void __fastcall TfrmEditorWindow::DrawView()
         StretchBlt(imgView->Picture->Bitmap->Canvas->Handle, 0, 0, imgView->Width, imgView->Height, m_View->Canvas->Handle, 0, 0, m_View->Width, m_View->Height, SRCCOPY);
 
         // draw the character grid
-        imgView->Picture->Bitmap->Canvas->Pen->Color = (TColor)0x00004080;   // a kind of light brown/yellow
+        imgView->Picture->Bitmap->Canvas->Pen->Color = ThemeManager::Shadow;
         for (auto y = 0; y < m_View->Height; y++)
         {
             imgView->Picture->Bitmap->Canvas->MoveTo(0, y * m_Scalar);
             imgView->Picture->Bitmap->Canvas->LineTo(imgView->Picture->Bitmap->Width, y * m_Scalar);
         }
+        imgView->Picture->Bitmap->Canvas->MoveTo(0, m_View->Height * m_Scalar - 1);
+        imgView->Picture->Bitmap->Canvas->LineTo(imgView->Picture->Bitmap->Width, m_View->Height * m_Scalar - 1);
         for (auto x = 0; x < m_View->Width; x++)
         {
             imgView->Picture->Bitmap->Canvas->MoveTo(x * m_Scalar, 0);
             imgView->Picture->Bitmap->Canvas->LineTo(x * m_Scalar, imgView->Picture->Bitmap->Height);
         }
+        imgView->Picture->Bitmap->Canvas->MoveTo(m_View->Width * m_Scalar - 1, 0);
+        imgView->Picture->Bitmap->Canvas->LineTo(m_View->Width * m_Scalar - 1, imgView->Picture->Bitmap->Height);
     }
     sbrWindow->Panels->Items[0]->Text = "Left: "   + IntToStr(m_Document->Left  );
     sbrWindow->Panels->Items[1]->Text = "Top: "    + IntToStr(m_Document->Top   );
