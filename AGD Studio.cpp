41a57955d2739dfcc6d49af315b3b20dcd9b7002
Kiwi73 <tonyt73@gmail.com>
2019-10-06 20:02:06 +1000

    Project fixes
    Remove the "Manage project paths" option from the project
    Now need to reference headers relative to the project folder

--- AGD Studio.cpp
+++ AGD Studio.cpp
@@ -11,32 +11,32 @@
 #include <windows.h>
 //---------------------------------------------------------------------------
 //USEFORM("Forms\fNewImage.cpp", frmNewImage);
+USEFORM("Frames\WindowEditor\fEditorWindow.cpp", frmEditorWindow); /* TFrame: File Type */
 USEFORM("Frames\WelcomeDialog\fSelectionPanel.cpp", SelectionPanelFrame); /* TFrame: File Type */
 USEFORM("Frames\WelcomeDialog\fWelcomeDialog.cpp", frmWelcomeDialog); /* TFrame: File Type */
-USEFORM("Frames\WindowEditor\fEditorWindow.cpp", frmEditorWindow); /* TFrame: File Type */
-USEFORM("Frames\MapEditor\LabelledImage.cpp", frmLabelledImage); /* TFrame: File Type */
-USEFORM("Frames\JumpEditor\fEditorJumpTable.cpp", frmEditorJump); /* TFrame: File Type */
+USEFORM("Frames\MusicEditor\fEditorMusic.cpp", frmEditorMusic); /* TFrame: File Type */
 USEFORM("Frames\MapEditor\AssetSelection.cpp", frmAssetSelection); /* TFrame: File Type */
 USEFORM("Frames\MapEditor\fEditorMap.cpp", frmEditorMap); /* TFrame: File Type */
-USEFORM("Frames\MusicEditor\fEditorMusic.cpp", frmEditorMusic); /* TFrame: File Type */
+USEFORM("Frames\MapEditor\LabelledImage.cpp", frmLabelledImage); /* TFrame: File Type */
+USEFORM("Frames\ImageEditor\fToolbarShape.cpp", frmToolbarShape); /* TFrame: File Type */
 USEFORM("Frames\ImageEditor\fULAplusBitmap.cpp", frmULAplusBitmap); /* TFrame: File Type */
+USEFORM("Frames\JumpEditor\fEditorJumpTable.cpp", frmEditorJump); /* TFrame: File Type */
 USEFORM("Frames\ImageEditor\fBlockTypes.cpp", frmBlockTypes); /* TFrame: File Type */
 USEFORM("Frames\ImageEditor\fEditorImage.cpp", frmEditorImage); /* TFrame: File Type */
-USEFORM("Frames\ImageEditor\fMultiImageView.cpp", MultiImageViewFrame); /* TFrame: File Type */
+USEFORM("Frames\ImageEditor\fPaletteMonoAttribute.cpp", frmPaletteMonoAttribute); /* TFrame: File Type */
 USEFORM("Frames\ImageEditor\fSelectionImage.cpp", SelectionImageFrame); /* TFrame: File Type */
 USEFORM("Frames\ImageEditor\fToolbar.cpp", frmToolbar); /* TFrame: File Type */
-USEFORM("Frames\ImageEditor\fToolbarShape.cpp", frmToolbarShape); /* TFrame: File Type */
+USEFORM("Frames\ImageEditor\fMultiImageView.cpp", MultiImageViewFrame); /* TFrame: File Type */
 USEFORM("Frames\ImageEditor\fPaletteAttribute.cpp", frmPaletteAttribute); /* TFrame: File Type */
 USEFORM("Frames\ImageEditor\fPaletteBitmap.cpp", frmPaletteBitmap); /* TFrame: File Type */
-USEFORM("Frames\ImageEditor\fPaletteMonoAttribute.cpp", frmPaletteMonoAttribute); /* TFrame: File Type */
+USEFORM("Forms\fMain.cpp", frmMain);
 USEFORM("Forms\fNewImage.cpp", frmNewImage);
 USEFORM("Forms\fSettings.cpp", frmSettings);
-USEFORM("Frames\CodeEditor\fEditorCode.cpp", frmEditorCode); /* TFrame: File Type */
 USEFORM("Forms\fAbout.cpp", frmAbout);
-USEFORM("Forms\fMain.cpp", frmMain);
+USEFORM("Frames\IDE\fIDE.cpp", frmIDE); /* TFrame: File Type */
+USEFORM("Frames\CodeEditor\fEditorCode.cpp", frmEditorCode); /* TFrame: File Type */
 USEFORM("Frames\ControlsEditor\fEditorControls.cpp", frmEditorControls); /* TFrame: File Type */
 USEFORM("Frames\ControlsEditor\fKeyCapture.cpp", frmKeyCode); /* TFrame: File Type */
-USEFORM("Frames\IDE\fIDE.cpp", frmIDE); /* TFrame: File Type */
 //---------------------------------------------------------------------------
 int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE, LPTSTR, int)
 {
@@ -46,7 +46,7 @@ int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE, LPTSTR, int)
         Application->MainFormOnTaskBar = true;
         ThemeManager::SetStyle(appSettings.ActiveStyle.Trim() != "" ? appSettings.ActiveStyle : "Onyx Blue");
         Application->CreateForm(__classid(TfrmMain), &frmMain);
-        Application->Run();
+         Application->Run();
     }
     catch (Exception &exception)
     {
