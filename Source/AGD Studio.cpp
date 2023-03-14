4b9d7be7272a07abd5ec0d46590547f8425ff6b3
tonyt73@gmail <tonyt73@gmail.com>
2023-03-14 19:02:20 +1000

    rearranged repo folders

--- Source/AGD Studio.cpp
+++ Source/AGD Studio.cpp
@@ -10,15 +10,15 @@
 #include <assert.h>
 #include <windows.h>
 //---------------------------------------------------------------------------
+USEFORM("Frames\WelcomeDialog\fWelcomeDialog.cpp", frmWelcomeDialog); /* TFrame: File Type */
+USEFORM("Frames\WindowEditor\fEditorWindow.cpp", frmEditorWindow); /* TFrame: File Type */
+USEFORM("Frames\MapEditor\AssetSelection.cpp", frmAssetSelection); /* TFrame: File Type */
+USEFORM("Frames\MapEditor\fEditorMap.cpp", frmEditorMap); /* TFrame: File Type */
+USEFORM("Frames\MapEditor\LabelledImage.cpp", frmLabelledImage); /* TFrame: File Type */
+USEFORM("Frames\MusicEditor\fEditorMusic.cpp", frmEditorMusic); /* TFrame: File Type */
+USEFORM("Frames\WelcomeDialog\fSelectionPanel.cpp", SelectionPanelFrame); /* TFrame: File Type */
+USEFORM("Frames\MessagesEditor\fEditorMessages.cpp", frmEditorMessage); /* TFrame: File Type */
 USEFORM("Forms\fAbout.cpp", frmAbout);
-USEFORM("Forms\fAdvancedSettings.cpp", frmAdvancedSettings);
-USEFORM("Forms\fMain.cpp", frmMain);
-USEFORM("Forms\fNewImage.cpp", frmNewImage);
-USEFORM("Forms\fSettings.cpp", frmSettings);
-USEFORM("Frames\CodeEditor\fEditorCode.cpp", frmEditorCode); /* TFrame: File Type */
-USEFORM("Frames\ControlsEditor\fEditorControls.cpp", frmEditorControls); /* TFrame: File Type */
-USEFORM("Frames\ControlsEditor\fKeyCapture.cpp", frmKeyCode); /* TFrame: File Type */
-USEFORM("Frames\IDE\fIDE.cpp", frmIDE); /* TFrame: File Type */
 USEFORM("Frames\ImageEditor\fEditorImage.cpp", frmEditorImage); /* TFrame: File Type */
 USEFORM("Frames\ImageEditor\fMultiImageView.cpp", MultiImageViewFrame); /* TFrame: File Type */
 USEFORM("Frames\ImageEditor\fPaletteAttribute.cpp", frmPaletteAttribute); /* TFrame: File Type */
@@ -26,18 +26,18 @@ USEFORM("Frames\ImageEditor\fBlockTypes.cpp", frmBlockTypes); /* TFrame: File Ty
 USEFORM("Frames\ImageEditor\fPaletteBitmap.cpp", frmPaletteBitmap); /* TFrame: File Type */
 USEFORM("Frames\ImageEditor\fToolbarShape.cpp", frmToolbarShape); /* TFrame: File Type */
 USEFORM("Frames\ImageEditor\fULAplusBitmap.cpp", frmULAplusBitmap); /* TFrame: File Type */
+USEFORM("Frames\JumpEditor\fEditorJumpTable.cpp", frmEditorJump); /* TFrame: File Type */
 USEFORM("Frames\ImageEditor\fPaletteMonoAttribute.cpp", frmPaletteMonoAttribute); /* TFrame: File Type */
 USEFORM("Frames\ImageEditor\fSelectionImage.cpp", SelectionImageFrame); /* TFrame: File Type */
 USEFORM("Frames\ImageEditor\fToolbar.cpp", frmToolbar); /* TFrame: File Type */
-USEFORM("Frames\JumpEditor\fEditorJumpTable.cpp", frmEditorJump); /* TFrame: File Type */
-USEFORM("Frames\MapEditor\AssetSelection.cpp", frmAssetSelection); /* TFrame: File Type */
-USEFORM("Frames\MapEditor\fEditorMap.cpp", frmEditorMap); /* TFrame: File Type */
-USEFORM("Frames\MapEditor\LabelledImage.cpp", frmLabelledImage); /* TFrame: File Type */
-USEFORM("Frames\MessagesEditor\fEditorMessages.cpp", frmEditorsMessage); /* TFrame: File Type */
-USEFORM("Frames\MusicEditor\fEditorMusic.cpp", frmEditorMusic); /* TFrame: File Type */
-USEFORM("Frames\WelcomeDialog\fWelcomeDialog.cpp", frmWelcomeDialog); /* TFrame: File Type */
-USEFORM("Frames\WelcomeDialog\fSelectionPanel.cpp", SelectionPanelFrame); /* TFrame: File Type */
-USEFORM("Frames\WindowEditor\fEditorWindow.cpp", frmEditorWindow); /* TFrame: File Type */
+USEFORM("Forms\fSettings.cpp", frmSettings);
+USEFORM("Frames\CodeEditor\fEditorCode.cpp", frmEditorCode); /* TFrame: File Type */
+USEFORM("Frames\ControlsEditor\fEditorControls.cpp", frmEditorControls); /* TFrame: File Type */
+USEFORM("Forms\fAdvancedSettings.cpp", frmAdvancedSettings);
+USEFORM("Forms\fMain.cpp", frmMain);
+USEFORM("Forms\fNewImage.cpp", frmNewImage);
+USEFORM("Frames\ControlsEditor\fKeyCapture.cpp", frmKeyCode); /* TFrame: File Type */
+USEFORM("Frames\IDE\fIDE.cpp", frmIDE); /* TFrame: File Type */
 //---------------------------------------------------------------------------
 int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE, LPTSTR, int)
 {
@@ -45,7 +45,7 @@ int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE, LPTSTR, int)
     {
          Application->Initialize();
          Application->MainFormOnTaskBar = true;
-         ThemeManager::SetStyle(appSettings.ActiveStyle.Trim() != "" ? appSettings.ActiveStyle : "Windows 10 Black Pearl");
+         ThemeManager::SetStyle(appSettings.ActiveStyle.Trim() != "" ? ppSettings.ActiveStyle : "Windows 10 Black Pearl");
          Application->CreateForm(__classid(TfrmMain), &frmMain);
          Application->Run();
     }
