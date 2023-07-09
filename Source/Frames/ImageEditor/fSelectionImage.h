87027674df7ad6950f67fec7aca7f80193ec977c
tonyt73@gmail <tonyt73@gmail.com>
2023-07-09 10:25:14 +1000

    Update fSelectionImage.h
    formatting

--- Source/Frames/ImageEditor/fSelectionImage.h
+++ Source/Frames/ImageEditor/fSelectionImage.h
@@ -17,25 +17,25 @@ __published:// IDE-managed Components
     void __fastcall imgBitmapClick(TObject *Sender);
 private:    // User declarations
     const Agdx::GraphicsBuffer& m_Image;
-    String              m_Hint;
-    bool                m_Selected;
-    int                 m_Scale;
-    float               m_ScalarX;
-    float               m_ScalarY;
-    TNotifyEvent        FOnClick;
+    String                      m_Hint;
+    bool                        m_Selected;
+    int                         m_Scale;
+    float                       m_ScalarX;
+    float                       m_ScalarY;
+    TNotifyEvent                FOnClick;
 
-    void    __fastcall  SetSelected(bool state);
-    void    __fastcall  SetScale(int scale);
+    void    __fastcall          SetSelected(bool state);
+    void    __fastcall          SetScale(int scale);
 public:     // User declarations
-            __fastcall  TSelectionImageFrame(TComponent* Owner, const Agdx::GraphicsBuffer& image, const String& hint = "");
+            __fastcall          TSelectionImageFrame(TComponent* Owner, const Agdx::GraphicsBuffer& image, const String& hint = "");
 
-    void    __fastcall  Update();
+    void    __fastcall          Update();
 
-    bool    __property  Selected = { read = m_Selected, write = SetSelected };
-    int     __property  Scale = { read = m_Scale, write = SetScale };
+    bool    __property          Selected        = { read = m_Selected, write = SetSelected };
+    int     __property          Scale           = { read = m_Scale   , write = SetScale    };
 
             // click events
-            __property  TNotifyEvent OnSelectedClick = { read = FOnClick, write = FOnClick };
+        __property TNotifyEvent OnSelectedClick = { read = FOnClick  , write = FOnClick    };
 };
 //---------------------------------------------------------------------------
 #endif
