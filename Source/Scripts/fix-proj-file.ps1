4f5ef3d798bd57ce256c1d880623b578f84da0f9
Kiwi73 <tonyt73@gmail.com>
2024-10-18 19:41:31 +1000

    Updated some scripts

--- Source/Scripts/fix-proj-file.ps1
+++ Source/Scripts/fix-proj-file.ps1
@@ -1,3 +1,42 @@
+function Find-XmlProperty {
+    param (
+        [String]$File,
+        [String]$Property
+    )
+
+    if (Test-Path -Path $File) {
+        $lines = Get-Content -Path $File
+    }
+}
+
+function Sort-Lines {
+    param (
+        [String[]]$Lines,
+        [String]$SplitSectionsBy
+    )
+
+    # break it into sections
+    $preSection = @()
+    $postSection = @()
+    $sortSection = @()
+    $inPreSection = $true
+    $Lines.foreach({
+        if ($_.contains($SplitSectionsBy)) {
+            $inPreSection = $false
+            $sortSection += $_
+        } elseif ($inPreSection) {
+            $preSection += $_
+        } else {
+            $postSection += $_
+        }
+    })
+
+    # sort the form resources section
+    $sortSection = $sortSection | Sort-Object
+
+    $Lines = $preSection + $sortSection + $postSection
+    return $Lines
+}
 
 function Sort-File {
     param (
@@ -7,37 +46,22 @@ function Sort-File {
 
     if (Test-Path -Path $File) {
         $lines = Get-Content -Path $File
-
-        # break it into sections
-        $preSection = @()
-        $postSection = @()
-        $sortSection = @()
-        $inPreSection = $true
-        $lines.foreach({
-            if ($_.contains($SplitSectionsBy)) {
-                $inPreSection = $false
-                $sortSection += $_
-            } elseif ($inPreSection) {
-                $preSection += $_
-            } else {
-                $postSection += $_
-            }
-        })
-
-        # sort the form resources section
-        $sortSection = $sortSection | Sort-Object
-
-        # create a new cbproj file
-        Remove-Item -Path "$($File).old" -ErrorAction Ignore
-        Move-Item -Path $File -Destination "$($File).old" 
-        $preSection | Out-File -FilePath $file
-        $sortSection | Out-File -FilePath $file -Append
-        $postSection | Out-File -FilePath $file -Append
+        Sort-Lines -Lines $lines -SplitSectionsBy $SplitSectionsBy | Out-File -FilePath $File
     }
 }
 
-$projFile = Get-ChildItem -Filter "*.cbproj"
-Sort-File -File $projFile.FullName -SplitSectionsBy "<FormResources"
 
-$cppFile = Get-ChildItem -Filter "$($projFile.BaseName).cpp"
-Sort-File -File $cppFile.FullName -SplitSectionsBy "USEFORM("
+$projFile = Get-ChildItem -Filter "*.cbproj"
+# read the file
+if (Test-Path -Path $projFile) {
+    $lines = Get-Content -Path $projFile
+    $lines = Sort-Lines -Lines $lines -SplitSectionsBy "<FormResources"
+
+    # create a new cbproj file
+    Remove-Item -Path "$($projFile.BaseName).old" -ErrorAction Ignore
+    Move-Item -Path $projFile -Destination "$($projFile.BaseName).old"
+    $lines | Out-File -FilePath $projFile
+
+    $cppFile = Get-ChildItem -Filter "$($projFile.BaseName).cpp"
+    Sort-File -File $cppFile.FullName -SplitSectionsBy "USEFORM("
+}
