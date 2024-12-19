a1e8ece26b21c04d7ed8ff076173ea1d3c6433e1
Kiwi73 <tonyt73@gmail.com>
2024-12-19 10:39:24 +1000

    Fixed a launch issue
    - funny, never had this issue before.

--- Source/Build/Emulation.cpp
+++ Source/Build/Emulation.cpp
@@ -26,12 +26,12 @@ bool __fastcall Emulation::Execute()
     auto emulator = Services::File::Resolve(Services::Folders::Application, mc.Emulator.Path);
     auto parameters = Services::Folders::CleanseSeparators(Parameter::ization(mc.Emulator.Parameters));
 
-    if (Services::File::Exists(parameters)) {
+    if (Services::File::Exists(StringReplace(parameters, "\"", "", TReplaceFlags() << rfReplaceAll))) {
         auto path = Services::Folders::CleanseSeparators(Services::File::PathOf(emulator));
         auto exe = Services::File::NameWithExtension(emulator);
         return ShellExecute(path, exe, parameters, false);
     }
-    BUILD_LINE(bmFailed, "Failed to find the emulator file to run: " + parameters);
+    BUILD_LINE(bmFailed, "Failed to find the emulation file to run game: " + parameters);
     return false;
 }
 //---------------------------------------------------------------------------
