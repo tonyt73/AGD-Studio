63b1535cac7ad7cfa45be27e93b2c56bf0f5c2bd
Kiwi73 <tonyt73@gmail.com>
2025-01-05 17:17:15 +1000

    Added a constants file for easier access

---
+++ Source/Project/Constants.h
@@ -0,0 +1,14 @@
+//---------------------------------------------------------------------------
+#ifndef ConstantsH
+#define ConstantsH
+//---------------------------------------------------------------------------
+namespace Project {
+const int g_MaxMapRoomsAcross  = 24;
+const int g_MaxMapRoomsDown    = 16;
+const int g_MaxRooms           = 254;
+const int g_RoomIndexDisabled  = 254;
+const int g_RoomIndexEmpty     = 255;
+const int g_RoomIndexInventory = 256;
+}
+//---------------------------------------------------------------------------
+#endif // ConstantsH
